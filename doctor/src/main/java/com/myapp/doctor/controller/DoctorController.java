707030c59c53a4024d1a66f0e6d5fc0af60b6c3e
valjeanjean <ilian.metina83@gmail.com>
2026-07-01 22:18:16 +0200

    adding rades for admin only

--- doctor/src/main/java/com/myapp/doctor/controller/DoctorController.java
+++ doctor/src/main/java/com/myapp/doctor/controller/DoctorController.java
@@ -1,10 +1,12 @@
 package com.myapp.doctor.controller;
 
+import com.myapp.doctor.dto.DoctorCreateRequest;
 import com.myapp.doctor.dto.DoctorCreateResponse;
 import com.myapp.doctor.dto.UserLoginRequest;
 import com.myapp.doctor.dto.DoctorLoginResponse;
 import com.myapp.doctor.service.DoctorService;
 import com.myapp.doctor.service.JwtService;
+import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,10 +26,12 @@ public class DoctorController {
 
     @PostMapping("/login")
     public DoctorLoginResponse login(@RequestBody UserLoginRequest dto){
-
+        return jwtService.login(dto);
     }
 
     @PostMapping("/create")
-    public DoctorCreateResponse createDoctor(@Valid)
+    public DoctorCreateResponse createDoctor(@Valid @RequestBody DoctorCreateRequest dto){
+        return doctorService.createDoctor(dto);
+    }
 
 }
--- doctor/src/main/java/com/myapp/doctor/service/JwtService.java
+++ doctor/src/main/java/com/myapp/doctor/service/JwtService.java
@@ -6,8 +6,14 @@ import com.myapp.doctor.dto.DoctorLoginResponse;
 import com.myapp.doctor.model.Doctor;
 import com.myapp.doctor.repository.DoctorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class JwtService {
@@ -15,6 +21,8 @@ public class JwtService {
     @Autowired
     private PasswordEncoder passwordEncoder;
     private final DoctorRepository repository;
+    @Value("${jwt.expiration}")
+    private Long jwtExpiration;
 
     public JwtService(DoctorRepository repository){
         this.repository = repository;
@@ -30,9 +38,31 @@ public class JwtService {
         UserInfoDTO userInfos = new UserInfoDTO();
         userInfos.setEmail(dto.getEmail());
         userInfos.setRole(doctor.getRole());
+
+
         DoctorLoginResponse userResponse = new DoctorLoginResponse();
 
 
+    }
+
+    public String generateToken(Doctor doctor){
+
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("id", doctor.getDoctorId());
+        claims.put("role", doctor.getRole());
+
+        String subject = doctor.getEmail();
+        Date issuedAt = new Date(System.currentTimeMillis());
+        Date expiration = new Date(System.currentTimeMillis() + jwtExpiration);
+
+        SecretKey key = getSignInKey();
+
+        return "";
+    }
+
+    private SecretKey getSignInKey() {
+        byte[] keyBytes = Decoder
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 
 
