7a9911c2d363b57bbc886a50d8b69fb65fd74ce8
Ilian Metina <ilian.metina83@gmail.com>
2026-07-01 16:07:46 +0200

    adds

--- doctor/src/main/java/com/myapp/doctor/controller/DoctorController.java
+++ doctor/src/main/java/com/myapp/doctor/controller/DoctorController.java
@@ -4,6 +4,7 @@ import com.myapp.doctor.dto.UserLoginRequest;
 import com.myapp.doctor.dto.UserResponse;
 import com.myapp.doctor.service.DoctorService;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
