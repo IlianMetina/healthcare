0656611ff49647f7a364a054b8c14ee9ec9d3334
Ilian Metina <ilian.metina83@gmail.com>
2026-07-03 14:25:09 +0200

    adding securityconfig methods

--- doctor/src/main/java/com/myapp/doctor/config/SecurityConfig.java
+++ doctor/src/main/java/com/myapp/doctor/config/SecurityConfig.java
@@ -1,15 +1,16 @@
 package com.myapp.doctor.config;
 
-import com.myapp.doctor.model.UserRoles;
-import com.myapp.doctor.repository.DoctorRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 public class SecurityConfig {
@@ -17,22 +18,37 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http
+                .csrf( csrf -> csrf.disable())
+                .cors(cors -> corsConfigurationSource())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/v1/users/login").permitAll()
                         .requestMatchers("/api/v1/users/create").hasRole("ADMIN")
                         .anyRequest().authenticated()
                 );
-
         return http.build();
     }
 
-    @Bean
-    public UserDetailsService userDetailsService(DoctorRepository doctorRepository){
-        return email -> doctorRepository.findDoctorByEmail(email).orElseThrow(() -> new UsernameNotFoundException("Email not found"));
-    }
+//    @Bean
+//    public UserDetailsService userDetailsService(DoctorRepository doctorRepository){
+//        return email -> doctorRepository.findDoctorByEmail(email).orElseThrow(() -> new UsernameNotFoundException("Email not found"));
+//    }
 
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource(){
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of("http://localhost:4200"));
+        config.setAllowedMethods(List.of("GET", "POST", "PATCH", "PUT", "DELETE"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return source;
     }
 }
