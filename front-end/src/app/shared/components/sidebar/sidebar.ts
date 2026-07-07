e1c7a8d7dc034df44b502ec14357d6f3a3e63b87
Ilian Metina <ilian.metina83@gmail.com>
2026-07-07 14:00:13 +0200

    adds

--- front-end/src/app/shared/components/sidebar/sidebar.ts
+++ front-end/src/app/shared/components/sidebar/sidebar.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Avatar } from '../avatar/avatar';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [Avatar],
+  imports: [Avatar, RouterLink],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.css',
 })
